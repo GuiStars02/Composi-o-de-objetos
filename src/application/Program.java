0823f266cd975f05e288f4b9d77dc8b0d1b1915d
Guilherme Andrade Campos <guilhermeandrade.2025@gmail.com>
2024-02-16 00:48:18 -0300

    Projeto composição de objetos simples

---
+++ src/application/Program.java
@@ -0,0 +1,68 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities_enums.OrderStatus;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter client data:");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.next();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birthDate = sdf.parse(sc.next());
+
+		Client client = new Client(name, email, birthDate);
+
+		System.out.print("Enter order data: ");
+		System.out.println("Status (Choose one below)");
+		System.out.print("PENDING_PAYMENT, PROCESSING, SHIPPED or DELIVERED: ");
+		OrderStatus status = OrderStatus.valueOf(sc.next());
+
+		System.out.println("How many items to this order? ");
+		int n = sc.nextInt();
+
+		Order order = new Order(new Date(), status, client);
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Enter #" + i + " item data:");
+			System.out.print("Product name: ");
+			sc.nextLine();
+			String productName = sc.nextLine();
+			System.out.print("Product price: ");
+			double productPrice = sc.nextDouble();
+			System.out.print("Quantity: ");
+			int quantity = sc.nextInt();		
+			
+			Product product = new Product(productName, productPrice);
+			
+			OrderItem it = new OrderItem(quantity, productPrice, product);
+			
+			order.addItem(it);
+		}
+		
+		System.out.println();
+		System.out.println(order);
+		
+		
+		sc.close();
+	}
+
+}
